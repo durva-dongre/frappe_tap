92abf3f543894672a6f5bc3c4b6dfacf625cfa2e
AMARTHA <amartha2704@gmail.com>
2025-08-14 18:14:57 +0530

    test

--- tap_lms/tests/test_learningunit.py
+++ tap_lms/tests/test_learningunit.py
@@ -3,38 +3,6 @@ import sys
 from unittest.mock import Mock, patch
 
 
-def test_learningunit_import_coverage():
-    """
-    Test to achieve 100% coverage for learningunit.py
-    Tests the LearningUnit class definition and all code paths
-    """
-    
-    # Use patch decorators to mock the imports at the function level
-    with patch.dict('sys.modules', {
-        'frappe': Mock(),
-        'frappe.model': Mock(),
-        'frappe.model.document': Mock(),
-    }):
-        # Mock the specific classes/functions we need
-        mock_frappe = sys.modules['frappe']
-        mock_document_class = Mock()
-        sys.modules['frappe.model.document'].Document = mock_document_class
-        
-        # Import after mocking to cover lines 4-5
-        from tap_lms.tap_lms.doctype.learningunit.learningunit import LearningUnit
-        
-        # Test class definition and inheritance (covers line 7)
-        assert issubclass(LearningUnit, mock_document_class)
-        
-        # Test class instantiation
-        learning_unit = LearningUnit()
-        assert isinstance(learning_unit, mock_document_class)
-        
-        # Test that the class has the pass statement covered (line 8)
-        # The pass statement is covered by simply defining the class
-        assert hasattr(LearningUnit, '__name__')
-        assert LearningUnit.__name__ == 'LearningUnit'
-
 
 
 
