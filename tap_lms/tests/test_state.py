35981586aa16e90263bfb5a5d59f3985d2843397
AMARTHA <amartha2704@gmail.com>
2025-08-17 14:11:51 +0530

    test

--- tap_lms/tests/test_state.py
+++ tap_lms/tests/test_state.py
@@ -23,23 +23,6 @@ def mock_frappe():
     }
 
 
-def test_import_statement_coverage(mock_frappe):
-    """Test coverage of line 5: from frappe.model.document import Document"""
-    with patch.dict('sys.modules', mock_frappe):
-        # Import the module to execute the import statement
-        import tap_lms.tap_lms.doctype.state.state
-        assert tap_lms.tap_lms.doctype.state.state is not None
-
-
-def test_class_definition_coverage(mock_frappe):
-    """Test coverage of line 7: class State(Document):"""
-    with patch.dict('sys.modules', mock_frappe):
-        from tap_lms.tap_lms.doctype.state.state import State
-        
-        # Verify class is defined
-        assert State is not None
-        assert hasattr(State, '__name__')
-        assert State.__name__ == 'State'
 
 
 def test_pass_statement_coverage(mock_frappe):
@@ -66,15 +49,6 @@ def test_complete_module_coverage(mock_frappe):
         assert instance is not None
 
 
-def test_class_inheritance(mock_frappe):
-    """Test that State properly inherits from Document"""
-    with patch.dict('sys.modules', mock_frappe):
-        from tap_lms.tap_lms.doctype.state.state import State
-        
-        # Check inheritance
-        mock_document = mock_frappe['frappe.model.document'].Document
-        assert mock_document in State.__bases__
-
 
 def test_instance_creation_with_data(mock_frappe):
     """Test creating State instance with data"""
@@ -93,21 +67,6 @@ def test_instance_creation_with_data(mock_frappe):
         
         assert instance is not None
 
-
-# Standalone test functions
-def test_import_coverage():
-    """Standalone test to cover import statement"""
-    mock_document = Mock()
-    mock_frappe = Mock()
-    mock_frappe.model.document.Document = mock_document
-    
-    with patch.dict('sys.modules', {
-        'frappe': mock_frappe,
-        'frappe.model': mock_frappe.model,
-        'frappe.model.document': mock_frappe.model.document
-    }):
-        import tap_lms.tap_lms.doctype.state.state
-        assert tap_lms.tap_lms.doctype.state.state is not None
 
 
 def test_class_coverage():
