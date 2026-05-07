26bafe240b06da93e8f602cf40d9d4d7c90468d3
Manu <manu.a@tap>
2026-05-07 17:50:48 +0530

    video-assignment link

---
+++ tap_lms/tap_lms/doctype/assignment_submission_media_link/assignment_submission_media_link.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Techt4dev and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Assignment Submission Media Link', {
+	// refresh: function(frm) {
+
+	// }
+});
