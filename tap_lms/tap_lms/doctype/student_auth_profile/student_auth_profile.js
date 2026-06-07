322976d91b2922e0ddec162f42c9a296c2284cc9
Durva Dongre <durvadongre@gmail.com>
2026-06-07 08:56:07 +0000

    Fixed Avtars

--- tap_lms/tap_lms/doctype/student_auth_profile/student_auth_profile.js
+++ tap_lms/tap_lms/doctype/student_auth_profile/student_auth_profile.js
@@ -1,7 +1,6 @@
 frappe.ui.form.on("Student Auth Profile", {
     student(frm, cdt, cdn) {
         const row = locals[cdt][cdn];
-
         if (!row.student) {
             row.student_name = "";
             frm.refresh_field("students");
--- tap_lms/tap_lms/doctype/student_auth_profile/student_auth_profile.py
+++ tap_lms/tap_lms/doctype/student_auth_profile/student_auth_profile.py
@@ -1,5 +1,10 @@
 import frappe
+from frappe.model.document import Document
 
-
-class StudentAuthProfile(frappe.model.document.Document):
-    pass
+class StudentAuthProfile(Document):
+    def validate(self):
+        if self.avatar:
+            val = str(self.avatar).strip()
+            if not val.isdigit() or not (1 <= int(val) <= 99):
+                frappe.throw("Avatar must be a number between 1 and 99")
+            self.avatar = val
