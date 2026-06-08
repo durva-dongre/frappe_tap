243bb684d3aa535548d9b28c2aa8e53b2fd7e7af
Durva Dongre <durvadongre@gmail.com>
2026-06-08 14:28:36 +0000

    Changed from Student to Citizenship

--- tap_lms/tap_lms/doctype/citizenship_auth/citizenship_auth.py
+++ tap_lms/tap_lms/doctype/citizenship_auth/citizenship_auth.py
@@ -1,5 +1,4 @@
 import frappe
 
-
 class CitizenshipAuth(frappe.model.document.Document):
 	pass
--- tap_lms/tap_lms/doctype/citizenship_auth_profile/citizenship_auth_profile.js
+++ tap_lms/tap_lms/doctype/citizenship_auth_profile/citizenship_auth_profile.js
@@ -1,9 +1,5 @@
 frappe.ui.form.on("Citizenship Auth Profile", {
-    citizenship_learner(frm, cdt, cdn) {
-        const row = locals[cdt][cdn];
-        if (!row.citizenship_learner) {
-            row.student_name = "";
-            frm.refresh_field("students");
-        }
+    refresh(frm) { 
+        
     }
 });
